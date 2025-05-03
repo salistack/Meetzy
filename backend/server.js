502919aea69b1a7e301cbcf6bf6913b8e7a3ba96
Umair Salih <101783935+salistack@users.noreply.github.com>
2025-05-03 13:24:40 +0530

    finnal file change

--- backend/server.js
+++ backend/server.js
@@ -159,4 +159,8 @@ const startServer = async () => {
   }
 };
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
 startServer();
